2b5255f3990f1bca297cbb10c724cc920de47fde
Robin <Larssonrobin88@gmail.com>
2022-10-26 15:55:52 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace NET22_OOP_8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Bla bla bla ");
+            Console.ReadKey();
+
+
+        }
+
+
+    }
+}

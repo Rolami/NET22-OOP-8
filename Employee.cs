8ece93bb2874e08edae5fa5248d83a9914204102
Robin <Larssonrobin88@gmail.com>
2022-10-26 16:27:18 +0200

    Adding employee class. Starting constructor.

---
+++ Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NET22_OOP_8
+{
+    internal class Employee
+    {
+
+        public int EmpID { get; set; } //TODO, Make seeded number
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; } //M,F,O
+        public double Salary { get; set; }
+
+
+        Employee()
+    }
+}
